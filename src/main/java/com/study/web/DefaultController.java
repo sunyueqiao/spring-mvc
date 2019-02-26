d3ca2a25bd055bebcdac7f25a1becb78556bbf26
sunyueqiao <sunyueqiao@jd.com>
2019-02-26 14:21:13 +0800

    加入了maven profile区分环境

--- src/main/java/com/study/web/DefaultController.java
+++ src/main/java/com/study/web/DefaultController.java
@@ -5,10 +5,15 @@ import com.study.service.UserInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 /**
  * DefaultController
@@ -19,18 +24,30 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/default")
 public class DefaultController {
-    private Logger logger = LoggerFactory.getLogger(DefaultController.class);
+    /**
+     *
+     */
+    private final static Logger LOGGER = LoggerFactory.getLogger(DefaultController.class);
+    /**
+     *
+     */
     @Autowired
     public UserInfoService userInfoService;
+    @Resource
+    public Environment standardEnvironment;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView index() {
         String name = "default/index";
-        logger.info("hello world! logback");
+        LOGGER.info("hello world! logback");
+        LOGGER.info(this.standardEnvironment.getActiveProfiles()[0]);
         ModelAndView model = new ModelAndView(name);
         UserInfo userInfo = this.userInfoService.getUserInfo(1);
-        model.addObject("msg", "hello world! by spring mvc");
-        model.addObject("userCount", userInfo.getUserName());
+        if (userInfo != null) {
+            model.addObject("msg", "hello world! by spring mvc");
+            model.addObject("userCount", userInfo.getUserName());
+        }
+
         return model;
     }
 }
